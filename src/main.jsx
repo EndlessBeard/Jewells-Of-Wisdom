51904911009539860612ca508fbb010deca697fe
EndlessBeard <endo@local.dev>
2025-09-25 12:49:54 -0400

    Use --page-text-color for text and add override for readable text on selected background

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/defaults.css'
+import './styles/generated-defaults-override.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
